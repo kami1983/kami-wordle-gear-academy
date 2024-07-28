c830d9a05e8d8ac5dde27141cee2c4184abf3d0f
kami1983 <kami@cancanyou.com>
2024-07-28 20:37:17 +0800

    Update homework

---
+++ wordle-session/tests/test.rs
@@ -0,0 +1,60 @@
+
+
+use gstd::ActorId;
+use gtest::{Log, Program, System};
+use io::{Action, Event, GameState, GameStatus, SessionStatus, StateQuery};
+
+const USER: u64 = 3;
+const TARGET_PROGRAME_ADDRESS: u64 = 2;
+
+
+#[test]
+fn test() {
+    let system = System::new();
+    system.init_logger();
+
+    let proxy_program = Program::current(&system);
+
+    let target_program = Program::from_file(&system, "../wordle-program/target/wasm32-unknown-unknown/debug/kami_wordle_gear_academy.opt.wasm");
+    let result = target_program.send_bytes(USER, []);
+    assert!(!result.main_failed());
+
+    println!("target_program = {:?}", target_program.id());
+
+    let target_program_address: ActorId = TARGET_PROGRAME_ADDRESS.into();
+    let result = proxy_program.send(USER, target_program_address);
+    assert!(!result.main_failed());
+
+    // Send a message to start the game Action::StartGame
+    let result = proxy_program.send(USER, Action::StartGame { user: USER.into() });
+    assert!(!result.main_failed());
+
+    println!(" Check result = {:?}", Event::GameStarted { user: USER.into() });
+    let log = Log::builder().source(1).dest(USER).payload(Event::GameStarted { user: USER.into() });
+    // let log = Log::builder().source(1).dest(USER).payload(Event::Ok);
+    println!("result = {:?}", &result);
+    println!("log = {:?}", &log);
+    assert!(result.contains(&log));
+
+    // Send a message to check the word
+    let result = proxy_program.send(USER, Action::CheckWord { user: USER.into(), word: "hxxxx".to_string() });
+    assert!(!result.main_failed());
+
+    // Check the logs to ensure the word was checked correctly
+    let log = Log::builder().source(1).dest(USER).payload(Event::WordChecked { user: USER.into(), correct_positions: vec![0], contained_in_word: vec![] });
+    assert!(result.contains(&log));
+
+    // Test state by proxy_program.read_state()
+    let wordle_program = system.get_program(1);
+
+    let state: GameState = wordle_program.read_state(StateQuery::GetGameState).expect("Unexpected invalid state.");
+    assert_eq!(state.game_status, GameStatus::InProgress);
+    assert_eq!(state.session_status, SessionStatus::Waiting);
+
+    let result = system.spend_blocks( 200);
+    let state: GameState = wordle_program.read_state(StateQuery::GetGameState).expect("Unexpected invalid state.");
+    assert_eq!(state.game_status, GameStatus::NotStarted);
+    assert_eq!(state.session_status, SessionStatus::Waiting);
+
+}
+
